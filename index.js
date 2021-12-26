0b4f5dd02c52e69caf9e40693d0a21b59b3e614e
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2021-12-26 19:44:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var n=prompt("emter number");
+ {
+   for(i=2;i<n/2;i++){
+    
+    if (n%i == 0)
+     {
+    break;
+     }
+    }
+     if(i==n/2)
+     alert("number is prime");
+     else 
+        alert("number is not prime");
+     }
+      
+   
+   
+  
+  
